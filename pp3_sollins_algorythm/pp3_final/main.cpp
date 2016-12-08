e2bfcf4035d685079a7817938faec814208341e3
alex960714 <alex_9607@mail.ru>
2016-12-08 15:55:20 +0300

    Added final version

--- pp3_sollins_algorythm/pp3_final/main.cpp
+++ pp3_sollins_algorythm/pp3_final/main.cpp
@@ -2,31 +2,30 @@
 #include "mpi.h"
 #include <fstream>
 #include <time.h>
-#include <stack>
+#include <stdlib.h>
 using namespace std;
 
-int *matr;
-int *mst_seq, *mst_par;
-int *comp;
-int *calc_en_vert;
+int *matr=nullptr;
+int *mst_seq=nullptr, *mst_par=nullptr;
+int *comp=nullptr;
+int *calc_en_vert=nullptr;
 int vert_num, comp_num;
 int f_opt_seq, f_opt_par;
 int edges_num;
 ofstream os;
 ifstream is;
-stack<int> curr_edges;
 
 int ProcNum, ProcRank;
-int *sendcounts, *displs;
-int *proc_matr;
-int *proc_calc_en_vert;
+int *sendcounts=nullptr, *displs=nullptr;
+int *proc_matr=nullptr;
+int *proc_calc_en_vert=nullptr;
 int proc_vert_num, proc_displs;
 
 double st_time_seq, en_time_seq;
 double st_time_par, en_time_par;
 MPI_Status status;
 
-void matr_init()
+void matr_init()                             //заполнение матрицы из файла
 {
 	is.open("adj_matr.txt");
 	is >> vert_num;
@@ -39,9 +38,9 @@ void matr_init()
 	is.close();
 }
 
-void matr_init_rand(int v_num, int power)
+void matr_init_rand(int v_num, int power)      //заполнение матрицы случайными числами
 {
-	srand(time(NULL));
+	srand(time(nullptr));
 	vert_num = v_num;
 	matr = new int[vert_num*vert_num];
 	for (int i = 0; i < vert_num; i++)
@@ -66,11 +65,11 @@ void matr_init_rand(int v_num, int power)
 	}
 }
 
-void mem_init()
+void mem_init()                                         //инициализация памяти
 {
 	if (!ProcRank)
 	{
-		matr_init_rand(9000, 1000);
+		matr_init_rand(8000, 1000);
 	}
 	MPI_Bcast(&vert_num, 1, MPI_INT, 0, MPI_COMM_WORLD);
 	comp = new int[vert_num];
@@ -107,7 +106,7 @@ void mem_init()
 
 
 
-void calculate()
+void calculate()                                         //вычисление минимального ребра для каждой вершины в процессе
 {
 	for (int i = 0; i < proc_vert_num; i++)
 	{
@@ -124,27 +123,9 @@ void calculate()
 	}
 }
 
-bool components_update(int* mst)
+bool components_update(int* mst,int &f_opt)             //вычисление минимального ребра для каждой компоненты - пересчёт принадлежности вершин компонентам
 {
 	bool update = false;
-	/*os << "Components:" << endl;
-	for (int i = 0; i < vert_num; i++)
-	{
-		os << comp[i] << " ";
-	}
-	os << endl << "Calculated vertexes:" << endl;
-	for (int i = 0; i < vert_num; i++)
-	{
-		os << calc_en_vert[i] << " ";
-	}
-	os << endl << "Current MST:" << endl;
-	for (int i = 0; i < vert_num; i++)
-	{
-		for (int j = 0; j < vert_num; j++)
-			os << mst[i*vert_num + j] << " ";
-		os << endl;
-	}
-	os << "-----------------------" << endl;*/
 	for (int i = 0; i < vert_num; i++)
 	{
 		if (calc_en_vert[i] != -1 && comp[i] != comp[calc_en_vert[i]])
@@ -174,7 +155,7 @@ bool components_update(int* mst)
 				comp[en_vert] = comp[st_vert];
 				comp_num--;
 				edges_num++;
-				f_opt_par += min;
+				f_opt += min;
 			}
 			else if (comp[st_vert] > comp[en_vert])
 			{
@@ -184,77 +165,32 @@ bool components_update(int* mst)
 				comp[st_vert] = comp[en_vert];
 				comp_num--;
 				edges_num++;
-				f_opt_par += min;
+				f_opt += min;
 			}
 		}
 	}
 	return update;
 }
 
-void sequential_version()
+void sequential_version()                          //последовательная версия
 {
-	f_opt_seq = 0;
-	//components = vert_num;
 	while (true)
 	{
 		for (int i = 0; i < vert_num; i++)
 		{
-			int min = _CRT_INT_MAX, st_vert = -1, en_vert = -1;
+			int min = _CRT_INT_MAX, en_vert = -1;
 			for (int j = 0; j < vert_num; j++)
 			{
-				if (comp[j] == i)
+				if (matr[i*vert_num + j] && comp[i] != comp[j] && matr[i*vert_num + j] < min)
 				{
-					for (int k = 0; k < vert_num; k++)
-					{
-						if (matr[k*vert_num+j] && comp[k] != comp[j] && matr[k*vert_num+j] < min)
-						{
-							min = matr[k*vert_num+j];
-							st_vert = j;
-							en_vert = k;
-						}
-					}
+					min = matr[i*vert_num + j];
+					en_vert = j;
 				}
 			}
-			if (en_vert != -1)
-			{
-				curr_edges.push(en_vert);
-				curr_edges.push(st_vert);
-			}
+			calc_en_vert[i] = en_vert;
 		}
-		if (curr_edges.empty())
+		if (!components_update(mst_seq, f_opt_seq))
 			break;
-		while (!curr_edges.empty())
-		{
-			int st_vert = curr_edges.top();
-			curr_edges.pop();
-			int en_vert = curr_edges.top();
-			curr_edges.pop();
-
-			if (mst_seq[st_vert*vert_num+en_vert] != matr[st_vert*vert_num + en_vert])
-			{
-				mst_seq[st_vert*vert_num + en_vert] = mst_seq[en_vert*vert_num+st_vert] = matr[st_vert*vert_num+en_vert];
-				//components--;
-				f_opt_seq += mst_seq[st_vert*vert_num+en_vert];
-				int curr_comp, new_comp;
-				if (comp[st_vert] > comp[en_vert])
-				{
-					curr_comp = comp[st_vert];
-					new_comp = comp[en_vert];
-					comp_num--;
-					vert_num++;
-				}
-				else
-				{
-					curr_comp = comp[en_vert];
-					new_comp = comp[st_vert];
-					comp_num--;
-					vert_num++;
-				}
-				for (int i = 0; i < vert_num; i++)
-					if (comp[i] == curr_comp)
-						comp[i] = new_comp;
-			}
-		}
 	}
 }
 
@@ -267,18 +203,21 @@ void opt_count(int* mst, int &f_opt)
 				f_opt += mst[i*vert_num + j];
 }
 
-bool check()
+bool check()                           //проверка эквивалентности полученных результатов
 {
-	if (f_opt_par != f_opt_seq)
-		return false;
+	/*if (f_opt_par != f_opt_seq)
+		return false;*/
 	for (int i = 0; i < vert_num; i++)
 		for (int j = i + 1; j < vert_num; j++)
 			if (mst_seq[i*vert_num + j] != mst_par[i*vert_num + j])
+			{
+				os << "Results are not equal in (i,j)" << endl;
 				return false;
+			}
 	return true;
 }
 
-void results_record()
+void results_record()                 //запись результатов
 {
 	if (vert_num < 30)
 	{
@@ -299,7 +238,9 @@ void results_record()
 		}
 	}
 
-	opt_count(mst_seq, f_opt_seq);
+	//opt_count(mst_seq, f_opt_seq);
+	os << "MST edges sum sequential: " << f_opt_seq << endl;
+	os << "MST edges sum parallel: " << f_opt_par << endl;
 	if (check())
 		os << "Results are equal" << endl;
 	else
@@ -310,9 +251,8 @@ void results_record()
 		os << "Calculated graph is spanning tree (forest)" << endl;
 	else
 		os << "Calculated graph is NOT spanning tree (forest). Results are not correct!" << endl;
-	opt_count(mst_par, f_opt_par);
-	os << endl << "MST edges sum: " << f_opt_par << endl;
-	os << "Sequential version time: " << en_time_seq - st_time_seq << endl;
+	//opt_count(mst_par, f_opt_par);
+	os << endl << "Sequential version time: " << en_time_seq - st_time_seq << endl;
 	os << "Parallel version time: " << en_time_par - st_time_par << endl;
 
 	double spdup = (en_time_seq - st_time_seq) / (en_time_par - st_time_par);
@@ -321,7 +261,7 @@ void results_record()
 	os.close();
 }
 
-void mem_del()
+void mem_del()               //освобождение памяти
 {
 	delete[] proc_matr;
 	delete[] comp;
@@ -351,19 +291,19 @@ int main(int argc, char** argv)
 
 	mem_init();
 
-	if (!ProcRank)
+	if (!ProcRank)                       //последовательная версия
 	{
 		f_opt_seq = 0;
 		st_time_seq = MPI_Wtime();
 		sequential_version();
-		en_time_seq = MPI_Wtime();
+		en_time_seq = MPI_Wtime();               //конец последовательной версии
 
 		for (int i = 0; i < vert_num; i++)
 			comp[i] = i;
 	}
 
 	MPI_Bcast(sendcounts, ProcNum, MPI_INT, 0, MPI_COMM_WORLD);
-	proc_matr = new int[sendcounts[ProcRank]];
+	proc_matr = new int[sendcounts[ProcRank]];                                    //распределение вершин между процессами
 	MPI_Scatterv(matr, sendcounts, displs, MPI_INT, proc_matr, sendcounts[ProcRank], MPI_INT, 0, MPI_COMM_WORLD);
 
 	if (!ProcRank)
@@ -382,19 +322,17 @@ int main(int argc, char** argv)
 	MPI_Bcast(displs, ProcNum, MPI_INT, 0, MPI_COMM_WORLD);
 	proc_vert_num = sendcounts[ProcRank];
 	proc_displs = displs[ProcRank];
-	/*MPI_Scatter(sendcounts, ProcNum, MPI_INT, &proc_vert_num, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	MPI_Scatter(displs, ProcNum, MPI_INT, &proc_displs, 1, MPI_INT, 0, MPI_COMM_WORLD);*/
 
 	if (ProcRank)
 		proc_calc_en_vert = new int[proc_vert_num];
 
 	MPI_Barrier(MPI_COMM_WORLD);
 	st_time_par = MPI_Wtime();
-	while (true)
+	while (true)                       //параллельная версия
 	{
 		MPI_Bcast(comp, vert_num, MPI_INT, 0, MPI_COMM_WORLD);
 
-		if (ProcRank)
+		if (ProcRank)                      
 		{
 			calculate();
 		}
@@ -403,13 +341,13 @@ int main(int argc, char** argv)
 		int update;
 		if (!ProcRank)
 		{
-			update = components_update(mst_par);
+			update = components_update(mst_par, f_opt_par);
 		}
 		MPI_Bcast(&update, 1, MPI_INT, 0, MPI_COMM_WORLD);
 		if (!update)
 			break;
-	}
-	MPI_Barrier(MPI_COMM_WORLD);
+	}                                     //конец параллельной версии
+	MPI_Barrier(MPI_COMM_WORLD); 
 	en_time_par = MPI_Wtime();
 
 	if (!ProcRank)
